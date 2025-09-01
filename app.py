5c7e406f9a05406047ed52a4f114c6c0229aa900
nivetha3305 <nivemurugesan335@gmail.com>
2025-09-02 00:49:08 +0530

    Fixed file extensions for Render

---
+++ app.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+# load model and encoders
+model = joblib.load("crop_model.pkl")
+label_encoders = joblib.load("encoders.pkl")
+
+app = FastAPI(title="🌱 Crop Recommendation API")
+
+# Input schema for validation
+class CropInput(BaseModel):
+    N: float
+    P: float
+    K: float
+    temperature: float
+    humidity: float
+    ph: float
+    rainfall: float
+
+@app.get("/")
+def home():
+    return {"message": "Crop Recommendation API is running 🚀"}
+
+@app.post("/predict")
+def predict(data: CropInput):
+    try:
+        features = [
+            data.N,
+            data.P,
+            data.K,
+            data.temperature,
+            data.humidity,
+            data.ph,
+            data.rainfall
+        ]
+
+        X = np.array(features).reshape(1, -1)
+        prediction = model.predict(X)[0]
+
+        return {"recommended_crop": str(prediction)}
+
+    except Exception as e:
+        return {"error": str(e)}
